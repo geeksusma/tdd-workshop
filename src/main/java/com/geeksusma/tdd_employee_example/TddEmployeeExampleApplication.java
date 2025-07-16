ea74be0a3bb32ab7ad102e55401580583b1bbe5c
Jesús María Villar Vázquez <jesus.maria.villar.vazquez@apipana.io>
2025-07-16 08:39:28 +0200

    initial commit

---
+++ src/main/java/com/geeksusma/tdd_employee_example/TddEmployeeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.geeksusma.tdd_employee_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddEmployeeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddEmployeeExampleApplication.class, args);
+	}
+
+}
